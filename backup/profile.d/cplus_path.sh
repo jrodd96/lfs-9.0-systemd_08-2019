8a51210a83de8ecf7c474bf822bab5adb8b474e3
Jared Stevens <jared.stevens1229@gmail.com>
2019-11-06 14:23:20 -0500

    Add files via upload

---
+++ backup/profile.d/cplus_path.sh
@@ -0,0 +1,6 @@
+# Begin /etc/profile.d/cplus_path.sh
+
+# Path for CPLUS_PATH if in the event error "could not find <stdlib.h>" occurs again
+CPLUS_INCLUDE_PATH=/usr/include/c++/9.2.0:/usr/include
+
+# End cplus_path.sh
---
+++ backup/profile.d/mvn.sh
@@ -0,0 +1,10 @@
+# Begin /etc/profile.d/mvn.sh
+
+JAVA_HOME=/opt/jdk/bin
+
+pathappend /opt/mvn/bin           PATH
+#pathappend /opt/mvn/lib/pkgconfig PKG_CONFIG_PATH
+
+export JAVA_HOME
+
+# End /etc/profile.d/mvn.sh
